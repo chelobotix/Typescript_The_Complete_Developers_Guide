3b834daa92934e9ed98817ece63a1ccdfe8a65db
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-21 10:32:16 -0400

    add arrays to file

--- Anotations/variables.ts
+++ Anotations/variables.ts
@@ -4,5 +4,24 @@ const control: boolean = true;
 const nothing: null = null;
 const nothing2: undefined = undefined;
 
-// build in objects
+// Build in objects
 const now: Date = new Date();
+
+// Arrays
+const color: string[] = ['blue', 'orenge', 'green'];
+const numbers: number[] = [1, 2, 3];
+
+// Classes
+class Car {}
+const car: Car = new Car();
+
+// Object
+const point: { fName: string; age: number } = {
+  fName: 'Natasha',
+  age: 5,
+};
+
+// Functions
+const sum: (a: number, b: number) => void = (a: number, b: number) => {
+  console.log(a + b);
+};

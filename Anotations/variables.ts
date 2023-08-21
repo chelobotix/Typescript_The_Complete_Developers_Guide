ee913b82e74ca9200447d80cee05cad2b6ac5e13
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-21 10:14:35 -0400

    Add varialbes.ts

---
+++ Anotations/variables.ts
@@ -0,0 +1,8 @@
+const apples: number = 5;
+const speed: string = 'Fast';
+const control: boolean = true;
+const nothing: null = null;
+const nothing2: undefined = undefined;
+
+// build in objects
+const now: Date = new Date();

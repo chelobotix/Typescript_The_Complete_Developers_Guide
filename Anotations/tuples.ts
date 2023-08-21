5f4287593e99e220eb48b42caa8554c2fb1f44f3
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-21 19:28:32 -0400

    Add touples.ts

---
+++ Anotations/tuples.ts
@@ -0,0 +1,8 @@
+const drink = {
+  color: 'brow',
+  isCarbonated: true,
+  sugar: 40,
+};
+
+// tuple is an array with specific order of types
+const pepsi: [string, boolean, number] = ['brow', true, 40];
